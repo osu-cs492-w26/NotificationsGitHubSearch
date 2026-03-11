bbaa36dd3a37b64e0f8564aab50c2fa6e15b22b6
Rob Hess <hessro@oregonstate.edu>
2026-03-11 13:13:41 -0700

    Add code from 3/11 lecture.

--- app/src/main/java/edu/oregonstate/cs492/notificationsgithubsearch/BookmarksSyncWorker.kt
+++ app/src/main/java/edu/oregonstate/cs492/notificationsgithubsearch/BookmarksSyncWorker.kt
@@ -1,11 +1,18 @@
 package edu.oregonstate.cs492.notificationsgithubsearch
 
+import android.Manifest
 import android.content.Context
 import android.util.Log
+import androidx.annotation.RequiresPermission
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.os.bundleOf
+import androidx.navigation.NavDeepLinkBuilder
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import edu.oregonstate.cs492.notificationsgithubsearch.data.AppDatabase
 import edu.oregonstate.cs492.notificationsgithubsearch.data.BookmarkedReposRepository
+import edu.oregonstate.cs492.notificationsgithubsearch.data.GitHubRepo
 import edu.oregonstate.cs492.notificationsgithubsearch.data.GitHubReposRepository
 import edu.oregonstate.cs492.notificationsgithubsearch.data.GitHubService
 
@@ -26,11 +33,40 @@ class BookmarksSyncWorker(
         for (repo in bookmarkedRepos) {
             val apiRepo = githubReposRepository.loadRepo(repo.name)
             if (apiRepo.isSuccess) {
+                sendNotification(apiRepo.getOrThrow())
                 bookmarkedReposRepository.updateBookmarkedRepo(
                     apiRepo.getOrThrow()
                 )
             }
         }
         return Result.success()
+    }
+
+    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
+    private fun sendNotification(repo: GitHubRepo) {
+        val builder = NotificationCompat.Builder(
+            applicationContext,
+            applicationContext.getString(R.string.notification_stars_channel)
+        )
+
+        val pendingIntent = NavDeepLinkBuilder(applicationContext)
+            .setGraph(R.navigation.main_nav_graph)
+            .setDestination(R.id.github_repo_detail)
+            .setArguments(bundleOf("repo" to repo))
+            .createPendingIntent()
+
+        builder.setContentTitle(applicationContext.getString(
+            R.string.notification_stars_title, repo.name
+        ))
+            .setContentText(applicationContext.getString(
+                R.string.notification_stars_text, repo.name, repo.stars
+            ))
+            .setSmallIcon(R.drawable.ic_github_logo)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        NotificationManagerCompat.from(applicationContext)
+            .notify(repo.name.hashCode(), builder.build())
     }
 }
--- app/src/main/java/edu/oregonstate/cs492/notificationsgithubsearch/GitHubSearchApplication.kt
+++ app/src/main/java/edu/oregonstate/cs492/notificationsgithubsearch/GitHubSearchApplication.kt
@@ -1,9 +1,14 @@
 package edu.oregonstate.cs492.notificationsgithubsearch
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.work.Constraints
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import java.util.concurrent.TimeUnit
@@ -12,23 +17,38 @@ class GitHubSearchApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         launchBookmarksSyncWorker()
+        createStarsNotificationChannel()
     }
 
     private fun launchBookmarksSyncWorker() {
-        val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.CONNECTED)
-            .build()
-        val workRequest = PeriodicWorkRequestBuilder<BookmarksSyncWorker>(
-            12,
-            TimeUnit.HOURS
-        )
-            .setConstraints(constraints)
-            .build()
+//        val constraints = Constraints.Builder()
+//            .setRequiredNetworkType(NetworkType.CONNECTED)
+//            .build()
+//        val workRequest = PeriodicWorkRequestBuilder<BookmarksSyncWorker>(
+//            12,
+//            TimeUnit.HOURS
+//        )
+//            .setConstraints(constraints)
+//            .build()
+//
+//        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+//            "bookmarksSyncWorker",
+//            ExistingPeriodicWorkPolicy.KEEP,
+//            workRequest
+//        )
+        val workRequest = OneTimeWorkRequest.from(BookmarksSyncWorker::class.java)
+        WorkManager.getInstance(this).enqueue(workRequest)
+    }
 
-        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
-            "bookmarksSyncWorker",
-            ExistingPeriodicWorkPolicy.KEEP,
-            workRequest
-        )
+    private fun createStarsNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                getString(R.string.notification_stars_channel),
+                getString(R.string.notification_stars_channel_title),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
